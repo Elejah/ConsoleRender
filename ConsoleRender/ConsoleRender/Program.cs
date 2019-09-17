4002cee09860bcd8a469ea0d0151848ab53ca631
Ilya Yurkevich <ilua.yurkevich.official@gmail.com>
2019-09-17 12:36:50 +0300

    add project files

---
+++ ConsoleRender/ConsoleRender/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace ConsoleRender
+{
+    class Program
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            OpenFileDialog openFileDialogImage = new OpenFileDialog();
+            if (openFileDialogImage.ShowDialog() == DialogResult.OK)
+            {
+                string fileName = openFileDialogImage.FileName;
+                string news = fileName.Replace(".drawio", ".xml");
+                Console.WriteLine(news);
+                Console.ReadLine();
+                LoadImage2();
+                LoadImage1(news);
+            }
+        }
+
+        static void LoadImage1(string path)
+        {
+            string strCmdText;
+            strCmdText = "/k drawio "+path+" -o test.png";
+            System.Diagnostics.Process.Start("CMD.exe", strCmdText);
+        }
+
+        static void LoadImage2()
+        {
+            string strCmdText;
+            strCmdText = "/k npm install --global draw.io - export";
+            System.Diagnostics.Process.Start("CMD.exe", strCmdText);
+        }
+    }
+}
